561838ac52a154c4fa77add248823570097d05af
Justus Kiplimo <59371869+jk45limo@users.noreply.github.com>
2022-01-05 21:32:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>Welcome to our movie site.</h1>
+        
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
